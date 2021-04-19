78d05c078864822a10f6a8180aa3aafe60641746
sfigraf <sfigraf@gmail.com>
2021-04-19 14:10:00 -0600

    framework set up

--- outdoor_map_app/app.R
+++ outdoor_map_app/app.R
@@ -1,47 +1,34 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+library(tidyverse)
+library(leaflet)
+library(geojsonio)
 library(shiny)
+places <- read.csv("places.csv")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
-)
+    mainPanel(
+        leafletOutput("map1", 
+                      width = 1800,
+                      height = 1000
+                      )
+        ) #end of tabpanel
+) #end of navbar page
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    output$map1 <- renderLeaflet({
+        places %>%
+            leaflet() %>%
+            addTiles(group = "Basemap") %>%
+            addProviderTiles('Esri.WorldImagery', group = "Satelite") %>%
+            addMarkers(lat = ~LAT, lng = ~LONG, 
+                       #popup = NOTES,
+                       label = ~NAME) %>%
+            addLayersControl(overlayGroups = c("Satelite", "Basemap"),
+                             options = layersControlOptions(collapsed = FALSE, title = "Layers"))
+            
+            
     })
 }
 
